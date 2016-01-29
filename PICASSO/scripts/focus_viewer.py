e14fb96c39a136908a1b3e6a4a72190a4eb7082b
fishnchips1624 <fishnchips1624@users.noreply.github.com>
2016-01-28 19:41:42 -0600

    fixed issue wiht duplicating plot

--- PICASSO/scripts/focus_viewer.py
+++ PICASSO/scripts/focus_viewer.py
@@ -7,7 +7,6 @@ import numpy as np
 import Image
 import TifImagePlugin
 from watchdog.observers import Observer
-from watchdog.events import PatternMatchingEventHandler
 from watchdog.events import FileSystemEventHandler
 from sys import argv, exit
 import time
@@ -16,6 +15,16 @@ import time
 mpl.rcParams['savefig.dpi'] = 400
 mpl.rcParams['figure.figsize'] = (8,5)
 
+
+class img:
+    def __init__(self,path,filename):
+        new = Image.open(path+'/'+filename)
+        self.size = new.size
+        self.data = np.reshape(new.getdata(), self.size)
+        self.pmax = prctile(new.getdata(),p=95)
+        self.pmin = prctile(new.getdata(),p=5)
+        self.title= 'New range = %i-%i'%(self.pmin,self.pmax)
+        self.label= 'UT: %s\nExp. Times: %.1f sec\nTemp: %.1fC' % (new.info['UniversalTime'].strftime('%m-%d-%y %H:%M:%S'),new.info['ExposureTime'],new.info['CCDTemperature'])
 
 class FileModifiedHandler(FileSystemEventHandler):
 
@@ -43,8 +52,9 @@ if __name__ == "__main__":
     filename = argv[2]
 
     # Setup Plots
-    fig, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
+    fig, (ax1, ax2) = plt.subplots(1, 2,sharey=False)
     fig.show()
+    plt.gcf().subplots_adjust(bottom=0.2)
     old = []
 
     print path,filename
@@ -56,32 +66,32 @@ if __name__ == "__main__":
 
     try:
         while True:
-            time.sleep(1)
+            time.sleep(2)
             if flag:
                 # move figures if not first image
                 if old:
                     print 'Moving Old Plot'
-                    pmin = prctile(old.getdata(),p=5)
-                    pmax = prctile(old.getdata(),p=95)
-                    ax1.imshow(np.reshape(old.getdata(), old.size),vmin=pmin,vmax=pmax,cmap = mpl.cm.gray)
-                    ax1.set_title('Old range = %i-%i'%(pmin,pmax))
-                    ax1.set_xlabel('UT: %s\nExp. Time: %.1f sec\nTemp: %.1fC' % (old.info['UniversalTime'].strftime('%m-%d-%y %H:%M:%S'),old.info['ExposureTime'],old.info['CCDTemperature']))
+                    ax1.imshow(old.data,vmin=old.pmin,vmax=old.pmax,cmap = mpl.cm.gray)
+                    ax1.set_xlim([0,old.size[1]])
+                    ax1.set_ylim([0,old.size[0]])
+                    ax1.set_title(old.title)
+                    ax1.set_xlabel(old.label)
+                    plt.draw()
+                    #time.sleep(2)
 
                 # add new image
                 print 'Adding New Plot'
-                new = Image.open(path+'/'+filename)
-                pmin = prctile(new.getdata(),p=5)
-                pmax = prctile(new.getdata(),p=95)   
-                ax2.imshow(np.reshape(new.getdata(), new.size),vmin=pmin,vmax=pmax,cmap = mpl.cm.gray)
-                ax2.set_title('New range = %i-%i'%(pmin,pmax))
-                ax2.set_xlabel('UT: %s\nExp. Times: %.1f sec\nTemp: %.1fC' % (new.info['UniversalTime'].strftime('%m-%d-%y %H:%M:%S'),new.info['ExposureTime'],new.info['CCDTemperature']))
-
-                # Plot stuff
-                old = new
-                plt.gcf().subplots_adjust(bottom=0.2)
-                plt.ion()
+                new = img(path,filename)
+                ax2.imshow(new.data,vmin=new.pmin,vmax=new.pmax,cmap = mpl.cm.gray)
+                ax2.set_xlim([0,new.size[1]])
+                ax2.set_ylim([0,new.size[0]])
+                ax2.set_title(new.title)
+                ax2.set_xlabel(new.label)
+                
+                #plt.ioff()
                 plt.draw()
                 flag = False
+                old = new
 
     except KeyboardInterrupt:
         observer.stop()
