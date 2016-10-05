7470e7add61e0bd11513fd1a057944863830d40b
fishnchips1624 <fishnchips1624@users.noreply.github.com>
2016-10-05 12:29:14 -0500

    GPSprocess update for CASES processing; fix bug

--- Python/modules/GPSprocess.py
+++ Python/modules/GPSprocess.py
@@ -141,79 +141,85 @@ def process_instr(inst,year,doy,do_DB=True):
     
     # Do Cases Processing
     elif instr == 'cases':
-                
+        
         # NOTE: Python code (originally from /programs/plot_CASES_day.py
+        try:
         
-        # Paths to use
-        datapath = '/rdata/airglow/gps/'+inst+'/streaming/'
-        pngpath = '/rdata/airglow/gps/results/'
-        archivepath = '/rdata/airglow/gps/'+inst+'/'+year+'/'
-        log_fname = '/rdata/airglow/gps/results/'+inst+'_'+site'.log'
-        #log_cmd = '/usr/bin/perl /usr/local/share/Python/programs/load_'+inst+'_'+site+'_logfile.pl'
-        
-        # Create the filename of the data to be parsed
-        fname = '{:s}dataout_{:04d}_{:03d}.bin'.format(datapath,year,doy) 
-        
-        # Run binflate
-        os.popen('/usr/local/sbin/binflate -i ' + fname)
-        
-        # Load txinfo.log file
-        txfname = 'txinfo.log'
-        txprn,txdn, el, az, txsystem = cases.load_txinfo(txfname)
-        
-        # Load scint.log file
-        s4fname = 'scint.log'
-        s4prn, s4dn, s4, s4system = cases.load_scint(s4fname)
-        
-        # Create plots
-        dn = datetime.date(YEAR,1,1)+datetime.timedelta(days=doy-1)
-        s4fname = '{:s}{:s}H.png'.format(pngpath,dn.strftime('%y%m%d'))
-        cases.plot_s4summary(txprn,txdn,el,az,txsystem,s4prn,s4dn,s4,s4system,s4fname)
-        
-        # Write the logfile
-        fid = open(log_fname,'w')
-        fid.writelines('Site,Instrument,StartUTTime,StopUTTime,SummaryImage,MovieFile\n')
-        fid.writelines('{:d},{:d},{:s},{:s},{:s}H.png'.format(site_id,inst_id,
-                        s4dn[0].strftime('%Y-%m-%d %H:%M:%S'),
-                        s4dn[-1].strftime('%Y-%m-%d %H:%M:%S'),
-                        dn.strftime('%y%m%d')))
-        fid.close()
-        
-        # Load the log file into the database
-        #os.popen(log_cmd)
-        if do_DB:
-            database([s4fname],datapath,startut,stoput,inst_id,site_id)
-        
-        # Move the data
-        os.popen('mv {:s}dataout*_{:04d}_{:03d}.bin .'.format(datapath,year,doy
-            ) )
-        tar = tarfile.open('{:s}.tgz'.format(dn.strftime('%y%m%d')), 'w:gz')
-        
-        tar.add('dataout_{:04d}_{:03d}.bin'.format(year,doy))
-        
-        if os.path.exists('dataoutiq_{:04d}_{:03d}.bin'.format(year,doy)):
-           tar.add('dataoutiq_{:04d}_{:03d}.bin'.format(year,doy))
-
-        tar.close()
-
-        # Clean up files
-        #os.popen('tar czvf {:s}.tgz dataout*_{:04d}_{:03d}.bin'.format(dn.strftime('%y%m%d'), YEAR, DOY))
-        os.popen('rm dataout*_{:04d}_{:03d}.bin'.format(year,doy))
-        os.popen('mv {:s}.tgz {:s}{:04d}'.format(dn.strftime('%y%m%d'),archivepath,year))
-        os.popen('rm channel.log')
-        os.popen('rm iono.log')
-        os.popen('rm navsol.log')
-        os.popen('rm scint.log')
-        os.popen('rm txinfo.log')
-
-        
-        # Send Error if Neither
-        else: 
-            subject = "!!! GPS Problem " + date
-            print subject
-            Emailer.emailerror(subject, 'Something weird has happened: '+inst)
+            # Paths to use
+            datapath = '/rdata/airglow/gps/'+inst+'/streaming/'
+            pngpath = '/rdata/airglow/gps/results/'
+            archivepath = '/rdata/airglow/gps/'+inst+'/'+year+'/'
+            log_fname = '/rdata/airglow/gps/results/'+inst+'_'+site+'.log'
+            #log_cmd = '/usr/bin/perl /usr/local/share/Python/programs/load_'+inst+'_'+site+'_logfile.pl'
             
-        os.chdir('/rdata/airglow/rx/')
+            # Create the filename of the data to be parsed
+            fname = '{:s}dataout_{:04d}_{:03d}.bin'.format(datapath,year,doy) 
+            
+            # Run binflate
+            os.popen('/usr/local/sbin/binflate -i ' + fname)
+            
+            # Load txinfo.log file
+            txfname = 'txinfo.log'
+            txprn,txdn, el, az, txsystem = cases.load_txinfo(txfname)
+            
+            # Load scint.log file
+            s4fname = 'scint.log'
+            s4prn, s4dn, s4, s4system = cases.load_scint(s4fname)
+            
+            # Create plots
+            dn = datetime.date(YEAR,1,1)+datetime.timedelta(days=doy-1)
+            s4fname = '{:s}{:s}H.png'.format(pngpath,dn.strftime('%y%m%d'))
+            cases.plot_s4summary(txprn,txdn,el,az,txsystem,s4prn,s4dn,s4,s4system,s4fname)
+            
+            # Write the logfile
+            fid = open(log_fname,'w')
+            fid.writelines('Site,Instrument,StartUTTime,StopUTTime,SummaryImage,MovieFile\n')
+            fid.writelines('{:d},{:d},{:s},{:s},{:s}H.png'.format(site_id,inst_id,
+                            s4dn[0].strftime('%Y-%m-%d %H:%M:%S'),
+                            s4dn[-1].strftime('%Y-%m-%d %H:%M:%S'),
+                            dn.strftime('%y%m%d')))
+            fid.close()
+            
+            # Load the log file into the database
+            #os.popen(log_cmd)
+            if do_DB:
+                database([s4fname],datapath,startut,stoput,inst_id,site_id)
+            
+            # Move the data
+            os.popen('mv {:s}dataout*_{:04d}_{:03d}.bin .'.format(datapath,year,doy
+                ) )
+            tar = tarfile.open('{:s}.tgz'.format(dn.strftime('%y%m%d')), 'w:gz')
+            
+            tar.add('dataout_{:04d}_{:03d}.bin'.format(year,doy))
+            
+            if os.path.exists('dataoutiq_{:04d}_{:03d}.bin'.format(year,doy)):
+                tar.add('dataoutiq_{:04d}_{:03d}.bin'.format(year,doy))
+            
+            tar.close()
+            
+            # Clean up files
+            #os.popen('tar czvf {:s}.tgz dataout*_{:04d}_{:03d}.bin'.format(dn.strftime('%y%m%d'), YEAR, DOY))
+            os.popen('rm dataout*_{:04d}_{:03d}.bin'.format(year,doy))
+            os.popen('mv {:s}.tgz {:s}{:04d}'.format(dn.strftime('%y%m%d'),archivepath,year))
+            os.popen('rm channel.log')
+            os.popen('rm iono.log')
+            os.popen('rm navsol.log')
+            os.popen('rm scint.log')
+            os.popen('rm txinfo.log')
+
+        except:
+            subject = "!!! Processing Error (\'" + inst +'\','+str(year)+','+str(doy)+') @ ' + site 
+            print subject
+            Emailer.emailerror(subject, 'Cases problem')
+        
+
+    # Send Error if Neither
+    else: 
+        subject = "!!! GPS Problem " + date
+        print subject
+        Emailer.emailerror(subject, 'Something weird has happened: '+inst)
+        
+    os.chdir('/rdata/airglow/rx/')
 
 
 def database(filenames,data_dir,startut,stoput,inst_id,site_id,send_to_madrigal=False):
@@ -266,7 +272,7 @@ def database(filenames,data_dir,startut,stoput,inst_id,site_id,send_to_madrigal=
     con.close()
 
 
-def plot_cases_day(YEAR,DOY):
+#def plot_cases_day(YEAR,DOY):
 
 
 def GPS_multiprocess(arg_list, num_processes):
