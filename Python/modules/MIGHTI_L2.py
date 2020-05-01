46c16cce5afba27932c1628286feddc8ca0818c2
bharding512 <bharding512@gmail.com>
2020-05-01 09:58:15 -0700

    v2.0 release of MIGHTI_L2.py
    - same as v1.32 but we're calling it v2.0 per SDC request

--- Python/modules/MIGHTI_L2.py
+++ Python/modules/MIGHTI_L2.py
@@ -9,8 +9,8 @@
 # These need to be manually changed, when necessary.
 # NOTE: When the major version is updated, you should change the History global attribute
 # in both the L2.1 and L2.2 netcdf files, to describe the change (if that's still the convention)
-software_version_major = 1 # Should only be incremented on major changes
-software_version_minor = 32 # [0-99], increment on ALL published changes, resetting when the major version changes
+software_version_major = 2 # Should only be incremented on major changes
+software_version_minor = 0 # [0-99], increment on ALL published changes, resetting when the major version changes
 __version__ = '%i.%02i' % (software_version_major, software_version_minor) # e.g., 2.03
 ####################################################################################################
 
