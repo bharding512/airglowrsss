7da60ab5eb50257f128dad9fb0b43e3fb5c4dae2
bharding512 <bharding512@gmail.com>
2020-10-21 10:58:46 -0700

    v4.00 release of MIGHTI_L2.py
    \- same as v3.04. Bumping new major version so that the new dataset is labeled v04

--- Python/modules/MIGHTI_L2.py
+++ Python/modules/MIGHTI_L2.py
@@ -9,8 +9,8 @@
 # These need to be manually changed, when necessary.
 # NOTE: When the major version is updated, you should change the History global attribute
 # in both the L2.1 and L2.2 netcdf files, to describe the change (if that's still the convention)
-software_version_major = 3 # Should only be incremented on major changes
-software_version_minor = 4 # [0-99], increment on ALL published changes, resetting when the major version changes
+software_version_major = 4 # Should only be incremented on major changes
+software_version_minor = 0 # [0-99], increment on ALL published changes, resetting when the major version changes
 __version__ = '%i.%02i' % (software_version_major, software_version_minor) # e.g., 2.03
 ####################################################################################################
 
