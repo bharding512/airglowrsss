8dca72332c21bd707297e4b6b012c4cf9b7699ea
Ben Galewsky <ben@peartreestudio.net>
2025-03-12 21:31:32 -0500

    Switch to pyproject.toml and move Sender.py to python module

---
+++ DataManagement/README.md
@@ -0,0 +1,10 @@
+# AirGlow-RSSS
+Tools to support the understanding the dynamics of the ionosphere using a
+combination of optical and radio techniques. This is accomplished through 
+instrument development, experimental campaigns, and data analysis. 
+[Our group](https://airglow.ece.illinois.edu/) is 
+part of the Remote Sensing & Space Science group in the Department of Electrical 
+and Computer Engineering. We are always looking for students interested in becoming 
+involved in our research, especially those with a background in signal/image 
+processing, plasma physics, or optical/radio instrument design.
+
---
+++ DataManagement/airglow/Sender.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+'''
+Script to send files back to Airglow
+  -s to add SITE
+
+History: 27 Sep 2012 - initial script written; Daniel J. Fisher (dfisher2@illinois.edu)
+         12 Feb 2014 - Updated to v3.0 - txtcheck; Daniel J. Fisher (dfisher2@illinois.edu)
+
+'''
+
+# Import required modules
+import os
+from glob import glob
+
+
+def main():
+    print("Hello World!")
+
+    # Set up correct folder options
+    folders = {
+        '/cygdrive/c/Sending/',
+        '/cygdrive/d/Sending/',
+        '/cygdrive/f/Sending/',
+        '/home/gps/Sending/',
+        '/home/airglow/Sending/',
+        '/home/scintmon/Sending/',
+        '/data/Sending/',
+        'C:/Sending/',
+        'D:/Sending/',
+        'F:/Sending/'
+    }
+
+    # destiny = 'tx@remote2.ece.illinois.edu:/rdata/airglow/rx/.'
+    destiny = 'tx@remote2.ece.illinois.edu:/home/tx/rx/.'
+
+    mfs = 70  # minimum file size to send
+
+    send(folders, destiny, mfs)
+
+
+def send(folders: set[str], destiny: str, mfs: int):
+    # Go through all possible folders for data
+    files = []
+    for f in folders:
+        files = files + glob(f+'*.tar.gz*')
+        files = files + glob(f+'*.txt')
+
+    # Send all files one by one; remove if sent successfully
+    for f in [fx for fx in files if os.stat(fx).st_size > mfs]:
+        os.system('chmod 774 '+f)
+        flag = os.system('scp ' + f + ' ' + destiny)
+        if flag == 0:
+            os.remove(f)
+            print('Completed...')
+
+    print('All Sending is now Complete!')
