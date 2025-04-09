3d450c44e706e6944433ecf88b22d8663b3e498b
Ben Galewsky <ben@peartreestudio.net>
2025-04-09 11:30:16 -0500

    Remove cursor

--- DataManagement/dagster/dagster_airglow/sensors.py
+++ DataManagement/dagster/dagster_airglow/sensors.py
@@ -51,55 +51,50 @@ def group_files_by_date(file_list):
 
 @dg.sensor(
     job_name="unzip_archive_job",
+    minimum_interval_seconds=1 * 60 * 60,  # 1 hour
 )
 def instrument_upload_sensor(context,
                              s3: S3ResourceNCSA
                              ):
     context.log.info("Instrument data upload")
-    last_processed_date = int(context.cursor) if context.cursor else 0
-    context.log.info(f"Last processed date: {last_processed_date}")
+
     files = group_files_by_date(
         list_files(EnvVar('DEST_BUCKET').get_value(), "raw", s3.get_client())
     )
 
-    sensor_files = []
-    sensor_date = None
+
     for data_date in sorted(files.keys()):
-        if data_date <= last_processed_date:
-            continue
         sensor_files = files[data_date]
         sensor_date = data_date
-        break
 
-    context.log.info(f"Found {len(sensor_files)} files on {sensor_date}")
-    context.update_cursor(str(sensor_date))
+        context.log.info(f"Found {len(sensor_files)} files on {sensor_date}")
 
-    # After processing, there can be just the .txt file for that date
-    if sensor_files and len(sensor_files) > 1:
-        cloud_file = {}
-        tar_gz_files = {}
+        # After processing, there can be just the .txt file for that date
+        if sensor_files and len(sensor_files) > 1:
+            cloud_file = {}
+            tar_gz_files = {}
 
-        for file in sensor_files:
-            filename = file.split('/')[-1]
-            site_code = filename.split('_')[1]
+            for file in sensor_files:
+                filename = file.split('/')[-1]
+                site_code = filename.split('_')[1]
 
-            if filename.startswith("Cloud_"):
-                cloud_file[site_code] = file
-            elif "tar.gz" in file:
-                if site_code in tar_gz_files:
-                    tar_gz_files[site_code].append(file)
-                else:
-                    tar_gz_files[site_code] = [file]
+                if filename.startswith("Cloud_"):
+                    cloud_file[site_code] = file
+                elif "tar.gz" in file:
+                    if site_code in tar_gz_files:
+                        tar_gz_files[site_code].append(file)
+                    else:
+                        tar_gz_files[site_code] = [file]
 
-        for site in tar_gz_files.keys():
-            run_config = RunConfig({
-                "unzip_chunked_archive": ChunkedArchiveConfig(
-                    site=site,
-                    observation_date=str(sensor_date),
-                    cloud_file=cloud_file[site],
-                    file_chunks=tar_gz_files[site]
+            for site in tar_gz_files.keys():
+                run_config = RunConfig({
+                    "unzip_chunked_archive": ChunkedArchiveConfig(
+                        site=site,
+                        observation_date=str(sensor_date),
+                        cloud_file=cloud_file[site],
+                        file_chunks=tar_gz_files[site]
+                    )
+                    }
                 )
-                }
-            )
-            print(f"{sensor_date}: {cloud_file[site]}, [{', '.join(tar_gz_files[site])}]")
-            yield dg.RunRequest(run_key=f"sort-{sensor_date}-{site}", run_config=run_config)
+                print(f"{sensor_date}: {cloud_file[site]}, [{', '.join(tar_gz_files[site])}]")
+                yield dg.RunRequest(run_key=f"sort-{sensor_date}-{site}", run_config=run_config)
